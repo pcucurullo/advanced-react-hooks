46a0f16dea086f0fe0ddcad65e54dfb8ef42409a
Paola Cucurullo <paolacucurullo@packlink.com>
2021-02-27 19:19:51 +0100

    make asyncCallback a useCallback to sort out dependencies and rerenders

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -27,7 +27,7 @@ function asyncReducer(state, action) {
   }
 }
 
-function useAsync (asyncCallback, initialState, dependencies) {
+function useAsync (asyncCallback, initialState) {
   const [state, dispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
@@ -49,22 +49,18 @@ function useAsync (asyncCallback, initialState, dependencies) {
         dispatch({type: 'rejected', error})
       },
     )
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies)
+  }, [asyncCallback])
   return state;
 }
 
 function PokemonInfo({pokemonName}) {
-  const state = useAsync(
-    () => {
-      if (!pokemonName) {
-        return
-      }
-      return fetchPokemon(pokemonName)
-    },
-    {status: pokemonName ? 'pending' : 'idle'},
-    [pokemonName],
-  )
+  const pokemonCallback = React.useCallback(() => {
+    if (!pokemonName) {
+      return
+    }
+    return fetchPokemon(pokemonName)
+  }, [pokemonName]);
+  const state = useAsync(pokemonCallback, {status: pokemonName ? 'pending' : 'idle'});
   const {data: pokemon, status, error} = state
 
   if (status === 'idle' || !pokemonName) {
